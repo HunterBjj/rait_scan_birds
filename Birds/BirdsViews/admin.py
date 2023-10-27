01fa84e88206c11ee846fbb51e1daffed7b7a8a7
Bjj <a.urmanow@mail.ru>
2023-10-27 23:20:36 +0700

    add func create user and birds

--- Birds/BirdsViews/admin.py
+++ Birds/BirdsViews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Birds
 
-#admin.site.register()
+
+admin.site.register(User)
+admin.site.register(Birds)
--- Birds/BirdsViews/views.py
+++ Birds/BirdsViews/views.py
@@ -1,7 +1,7 @@
 from hashlib import sha256
 from django.shortcuts import render
 from .models import User, Birds, ViewedUser
-
+from django.contrib.auth import authenticate
 """
 Основные функции:
 auth - процесс аутентфикации пользователя.
@@ -15,26 +15,35 @@ def auth(request):
         login = request.POST.get("login")
         password = request.POST.get("password")
         pswHash = sha256(password.encode('utf-8')).hexdigest()
-        SQLdata = User.objects.raw(f"SELECT login, password FROM User WHERE login = {login} AND password = {pswHash}")
-        if SQLdata is True:  # Если логин и пароль существуют, то переходим на основную форму, это процесс авторизации.
-            return render(request, 'viewBirds.html', {"login": login})
+        user = User.objects.filter(login=login, password=password)
+        if user:  # Если логин и пароль существуют, то переходим на основную форму, это процесс авторизации.
+            return render(request, 'viewBirds.html/', {"login": login})
         elif login is not None:
             ErorrMessage = "Incorrect username or password."
-            return render(request, 'auth.html', {"Error": ErorrMessage})
-    return render(request, 'auth.html')
+            return render(request, 'auth.html/', {"Error": ErorrMessage})
+    return render(request, 'auth.html/')
 
 
 
 #def userView(request):
 
 
+def rgstrUser(request):
+    if request.method == 'POST':
+        user = User()
+        user.login = request.POST.get("login")
+        user.password = request.POST.get("password")
+        user.mail = request.POST.get("email")
+
 def viewBirds(request):
     birds = Birds.objects.all()
-    return render(request, 'viewBirds.html', {"birds": birds})
+    return render(request, 'viewBirds.html/', {"birds": birds})
 
 
 def creatBirds(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        color = request.POST.get('color')
-        PNG = request.POST.get('PNG')
+        birds = Birds()
+        birds.name = request.POST.get('name')
+        birds.feather_color = request.POST.get('color')
+        birds.PNG = request.POST.get('PNG')
+        birds.save()
